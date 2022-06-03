60ecf0a835228700f36d9bd983a920417cdd0a10
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-06-03 14:08:02 +0300

    addFeatureOpenTextCell

--- ProjectSettingScreenIphone/DataGroup /SwitchCell.swift
+++ ProjectSettingScreenIphone/DataGroup /SwitchCell.swift
@@ -5,4 +5,18 @@
 //  Created by Alexander Panasenko on 03.06.2022.
 //
 
-import Foundation
+import UIKit
+
+public struct SwitchCell {
+    public let text: String
+    public let icon: String
+    public let backgroundColorIcon: UIColor
+    public let isOnByDefault: Bool
+    
+    public init(text: String, icon: String, backgroundColoricon: UIColor, isOnByDefault: Bool) {
+        self.text = text
+        self.icon = icon
+        self.backgroundColorIcon = backgroundColoricon
+        self.isOnByDefault = isOnByDefault
+    }
+}
