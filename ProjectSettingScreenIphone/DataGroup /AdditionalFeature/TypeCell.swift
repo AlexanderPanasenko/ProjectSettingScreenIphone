4b4d5649bbea2a65fb6d1bb002a7ccce08b62e87
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-06-03 15:52:30 +0300

    addFeatureTypeCell

--- ProjectSettingScreenIphone/DataGroup /AdditionalFeature/TypeCell.swift
+++ ProjectSettingScreenIphone/DataGroup /AdditionalFeature/TypeCell.swift
@@ -5,4 +5,8 @@
 //  Created by Alexander Panasenko on 03.06.2022.
 //
 
-import Foundation
+enum TypeCell {
+    case defaultCell(withData: Defaultcell)
+    case switchCell(withData: SwitchCell)
+    case openTextCell(withData: OpenTextCell)
+}
