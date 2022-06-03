35dc409d486f64ae7dc7fa1c6be0d1af29f35db9
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-06-03 14:08:56 +0300

    addFeatureOpenText

--- ProjectSettingScreenIphone/DataGroup /OpenTextCell.swift
+++ ProjectSettingScreenIphone/DataGroup /OpenTextCell.swift
@@ -5,4 +5,18 @@
 //  Created by Alexander Panasenko on 03.06.2022.
 //
 
-import Foundation
+import UIKit
+
+public struct OpenTextCell {
+    public let text: String
+    public let icon: String
+    public let backgroundColorIcon: UIColor
+    public let openText: String
+    
+    public init(text: String, icon: String, backgroundColorIcon: UIColor, openText: String) {
+        self.text = text
+        self.icon = icon
+        self.backgroundColorIcon = backgroundColorIcon
+        self.openText = openText
+    }
+}
